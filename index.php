4cf6f93975147fa57a109f3cc634aeb4deaf1103
romualdhournon66 <k2001@cegetel.net>
2018-12-17 20:11:15 +0100

    mon index

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="FR">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <title>PHP Partie 6 exercice 1</title>
+    </head>
+    <body>
+        <div class="container" id="page-top">
+            <div class="row">
+                <div class="col-xl-12">
+                    <h1 align="center">PHP Partie 6 exercice 1</h1>
+                    <h3 align="center">Exercice 1</h3>
+                    <p align="center">Faire une page index.php. Tester sur cette page que tous les paramètres de cette 
+                        URL existent et les afficher: index.php?lastname=Nemare&firstname=Jean</p>
+                </div>
+            </div>
+            <p align="center"><a href="index.php?lastname=Nemare&firstname=Jean">Tester ici !</a></p>
+        </div>
+    </body>
+</html>
+
